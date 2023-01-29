b5cfc7310c72b064fdbf6d9708a255c0f214f0f7
AlexSura <yellowjazz@mail.ru>
2023-01-29 21:29:05 +0500

    correct6

--- db/migrate/20230127051419_create_join_table_tests_users.rb
+++ db/migrate/20230127051419_create_join_table_tests_users.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableTestsUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :tests_users, id: false do |t|
-      t.references :tests, foreign_key: true
-      t.references :users, foreign_key: true
+      t.references :test, foreign_key: true
+      t.references :user, foreign_key: true
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 categories = Category.create([{title: 'junior'},{title: 'midle'}])
 tests = Test.create([{title: 'PHP', level: 1, category_id: categories[0].id}, {title: 'JS', level: 1, category_id: categories[0].id }, {title: 'React', level: 2, category_id: categories[1].id}])
-questions = Question.create([{body: "что то про тест написано но не точно"}])
-answers = Answer.create([{correct: true, body:'ответ на вопрос'}])
+questions = Question.create([{body: "что то про тест написано но не точно", test_id: tests[0].id}])
+answers = Answer.create([{correct: true, body:'ответ на вопрос', question_id: questions[0].id}])
 users = User.create([{name: 'Pavel', age: 22}, {name: 'Roma', age: 33}])
 
 
