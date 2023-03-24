24a49e794e129939b5ef33984a16c55115bfa570
AlexSura <yellowjazz@mail.ru>
2023-03-24 19:39:58 +0500

    cor24

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,14 +7,26 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  def self.arr_categories(name_category)
-    testt = Test.joins("JOIN categories ON tests.category_id = categories.id WHERE categories.title = '#{name_category}'")
-    testt.order(title: :desc).pluck("title")
+  def arr_categories(name_category)
+    x = "#{name_category}"
+    testt = Test.all.joins(:category).where(category: { title: x }).order(title: :desc)
   end
 end
+
+
+
+
+
+
 
 =begin
 Создайте метод класса в модели Test, который возвращает отсортированный по убыванию
  массив названий всех Тестов у которых Категория называется определённым образом 
  (название категории передается в метод в качестве аргумента). 
+ t= Test.new(title: 'php')
+ t2 = Test.new(title: 'JS')
+ c = Category.new(title: 'jun')
+  t.category = c
+  t2.category = c
+  t.arr_categories('jun')
 =end
