6bccb8ee6cad73fe3cf5cc98bb20041a03ffeb2d
AlexSura <yellowjazz@mail.ru>
2023-04-03 22:19:34 +0500

    correct migration

--- app/models/test.rb
+++ app/models/test.rb
@@ -13,13 +13,13 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :heavy, -> { where(level: 5..Float::INFINITY) }
-  scope :what_categories, -> (name_category) {joins(:category).where(category: { title: name_category })}
+  scope :test_categories, -> (name_category) {joins(:category).where(category: { title: name_category })}
   
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { greater_than: 0, only_integer: true }
 
   def self.arr_categories(name_category)
-    what_categories(name_category).order(title: :desc).pluck(:title)
+    test_categories(name_category).order(title: :desc).pluck(:title)
   end
 end
 
--- db/migrate/20230114170026_create_tests.rb
+++ db/migrate/20230114170026_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, null: false, :default => 0, uniqueness: { scope: :level } 
+      t.integer :level, null: false, :default => 0 
       t.references :category, foreign_key: true
         
       t.timestamps
