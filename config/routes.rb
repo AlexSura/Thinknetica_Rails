bff4ca930cdbbb07e35012dcadee3a9cedb531d4
AlexSura <yellowjazz@mail.ru>
2023-04-19 12:01:06 +0500

    routs_contoller_views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root 'tests#index'
+
+resources :tests, except: [:index], shallow: true do
+  resources :questions
+end
+
+
+
+
+
+
+  #resources :tests
+  
+  # resources :tests, only: %i[index show] 
+
+  #resources :tests do 
+  
+  #resources :account 
+
+  #resources :questions, shallow: true
+
+
+  #get '/tests/:id/start', to: 'tests#start'    
+
+  #get '/tests/:id/start', to: 'tasks#start'
+
 end
