109fecef2463d66cdc253435576c9e0384e347fd
AlexSura <yellowjazz@mail.ru>
2023-05-10 16:23:44 +0500

    correct1

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,7 +3,7 @@ module QuestionsHelper
     if question.new_record?
       "Create New '#{@test.title}' Question"
     else 
-      "Edit '#{questions.test.title}' Question"
+      link_to "Edit '#{question.test.title}'questions", edit_test_path(question) 
     end
   end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,8 +1,8 @@
 module TestsHelper
-  TAST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
 
   def test_level(test)
-    TAST_LEVELS[test.level] || hero
+    TEST_LEVELS[test.level] || hero
   end
 
   
